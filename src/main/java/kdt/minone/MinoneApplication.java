bc04ff051d832f98e785c4754709158486ed416a
jiyeon0926 <yeoni9094@naver.com>
2025-07-08 15:46:40 +0900

    init

---
+++ src/main/java/kdt/minone/MinoneApplication.java
@@ -0,0 +1,13 @@
+package kdt.minone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinoneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kdt/minone/MinoneApplicationTests.java
@@ -0,0 +1,13 @@
+package kdt.minone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
