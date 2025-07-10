4e64ea68700285249316156f773b91282a7c3a3a
alsqja <alsqha2626@gmail.com>
2025-07-10 17:12:03 +0900

    refactor: rename CitizenLoginReqDto.java to LoginReqDto
    #7

--- src/main/java/kdt/minone/domain/auth/dto/LoginReqDto.java
+++ src/main/java/kdt/minone/domain/auth/dto/LoginReqDto.java
@@ -6,7 +6,7 @@ import lombok.RequiredArgsConstructor;
 
 @Getter
 @RequiredArgsConstructor
-public class CitizenLoginReqDto {
+public class LoginReqDto {
 
     @NotBlank
     private final String email;
