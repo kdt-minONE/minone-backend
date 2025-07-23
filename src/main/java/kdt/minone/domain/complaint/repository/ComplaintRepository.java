88cf0c0956cab6ed467bdc1aeb6bccf029f26680
jiyeon0926 <yeoni9094@naver.com>
2025-07-24 06:19:15 +0900

    feat: ComplaintRepository에 findByIdOrElseThrow 메서드 추가

--- src/main/java/kdt/minone/domain/complaint/repository/ComplaintRepository.java
+++ src/main/java/kdt/minone/domain/complaint/repository/ComplaintRepository.java
@@ -2,6 +2,12 @@ package kdt.minone.domain.complaint.repository;
 
 import kdt.minone.domain.complaint.entity.Complaint;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
 
 public interface ComplaintRepository extends JpaRepository<Complaint, Long> {
+
+    default Complaint findByIdOrElseThrow(Long complaintId) {
+        return findById(complaintId).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "not found"));
+    }
 }
