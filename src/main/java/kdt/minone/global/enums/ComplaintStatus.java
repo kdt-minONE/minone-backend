46cc2fcb81e2cf9faa65367a35df209a33780894
jiyeon0926 <yeoni9094@naver.com>
2025-07-29 16:12:31 +0900

    refactor: ComplaintStatus Enum 예외처리
    - 유효하지 않은 상태에 대해 ResponseStatusException 발생

--- src/main/java/kdt/minone/global/enums/ComplaintStatus.java
+++ src/main/java/kdt/minone/global/enums/ComplaintStatus.java
@@ -1,5 +1,8 @@
 package kdt.minone.global.enums;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
+
 public enum ComplaintStatus {
 
     ASSIGNED("접수"),
@@ -7,6 +10,16 @@ public enum ComplaintStatus {
     OFFICIAL_ASSIGNED("담당자 배정"),
     COMPLETED("완료"),
     CANCELLED("접수 취소");
+
+    public static ComplaintStatus of(String name) {
+        for (ComplaintStatus status : values()) {
+            if (status.name().equals(name)) {
+                return status;
+            }
+        }
+
+        throw new ResponseStatusException(HttpStatus.NOT_FOUND, "not found");
+    }
 
     private final String description;
 
